abccae5e8e84095956dad612918491908e96fc54
Jiahe Zhou <z.jessie.jh@gmail.com>
2025-02-04 22:41:39 +1100

    function-001: complete initEventListener

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function initEventListener() {
+    const buttons = document.getElementsByClassName('button');
+        buttons.forEach (button => {
+            button.addEventListener('click', (event) => {
+                alert('Button %s clicked', event.target.id);
+        })
+    })
 }
 
 function initWindowDisplay() {
+   
 }
